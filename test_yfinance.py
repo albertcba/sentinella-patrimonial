126a880044c73571dbc63cd8ed12832725c29262
albertcba <albertcba@users.noreply.github.com>
2026-09-20 17:55:46 +0200

    Update test_yfinance.py

--- test_yfinance.py
+++ test_yfinance.py
@@ -1,3 +1,10 @@
+# import yfinance as yf
+# t = yf.Ticker("META")
+# print(t.options)
+
 import yfinance as yf
+
 t = yf.Ticker("META")
-print(t.options)
+chain_dec = t.option_chain("2026-12-18")
+print(chain_dec.calls.columns)
+print(chain_dec.calls.head())
